e925b222ea26ec3edcc661b1d3b69834a5651278
Amit <amitoved@gmail.com>
2021-11-24 09:11:50 +0200

    WIP

---
+++ constants.py
@@ -0,0 +1,5 @@
+BACKGROUND = 'background'
+FOREGROUND = 'foreground'
+
+classes = {BACKGROUND: 0,
+           FOREGROUND: 1}
--- test2.py
+++ test2.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 from tkinter import filedialog, colorchooser
 
+import numpy as np
 from PIL import ImageTk, Image, ImageDraw
+
+import constants
 
 
 class App:
@@ -14,7 +17,8 @@ class App:
         self.bg_last_y = None
         self.fg_last_x = None
         self.fg_last_y = None
-
+        self.class_val = None
+        self.annotations = {val: [] for val in constants.classes.values()}
         self.window = tk.Tk()
         self.window.after(100, self.selecting_file)
         self.window.mainloop()
@@ -32,58 +36,57 @@ class App:
         self.frame_tools = tk.Frame(self.window)
         self.frame_tools.pack()
 
-        self.background_button = tk.Button(self.frame_tools, text='background',
-                                          command=lambda: self.change_figure('background'))
+        self.background_button = tk.Button(self.frame_tools, text=constants.BACKGROUND,
+                                          command=lambda: self.change_class(constants.classes[constants.BACKGROUND]))
         self.background_button.pack(side='left')
-        self.foreground_button = tk.Button(self.frame_tools, text='foreground', command=lambda: self.change_figure('foreground'))
+        self.foreground_button = tk.Button(self.frame_tools, text=constants.FOREGROUND,
+                                          command=lambda: self.change_class(constants.classes[constants.FOREGROUND]))
         self.foreground_button.pack(side='left')
+
+        self.save_button = tk.Button(self.frame_tools, text='save', command=self.save)
+        self.save_button.pack(side='left')
+
         self.size_entry = tk.Entry(self.frame_tools)
         self.size_entry.bind('<Return>', self.change_size)
         self.size_entry.pack(side='left')
 
-        self.color_button = tk.Button(self.frame_tools, text='Color: #FF0000', command=self.change_color)
-        self.color_button.pack(side='left')
+        # self.color_button = tk.Button(self.frame_tools, text='Color: #FF0000', command=self.change_color)
+        # self.color_button.pack(side='left')
 
         self.canvas = tk.Canvas(self.window, width=self.width, height=self.height)
         self.canvas.pack()
-        # self.canvas.bind('<Button-1>', self.on_click)
         self.canvas.bind("<Button-1>", self.get_x_and_y)
         self.canvas.bind("<B1-Motion>", self.draw_smth)
 
         self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
 
-        self.save_button = tk.Button(self.window, text='Save', command=self.save)
-        self.save_button.pack()
 
-    def on_click(self, event):
-        if self.figure == 'background':
-            self.draw_background(event)
-        elif self.figure == 'foreground':
-            self.draw_foreground(event)
 
-    def draw_foreground(self, event):
-        if self.fg_last_x:
-            self.canvas.create_line((self.fg_last_x, self.fg_last_y, event.x, event.y), fill='red', width=10)
-        self.fg_last_x = event.x
-        self.fg_last_y = event.y
 
     def get_x_and_y(self, event):
-        self.fg_lastx, self.fg_lasty = event.x, event.y
+        self.last_x, self.last_y = event.x, event.y
 
     def draw_smth(self, event):
-        self.canvas.create_line((self.fg_lastx, self.fg_lasty, event.x, event.y),
-                           fill='red',
-                           width=2)
-        self.fg_lastx, self.fg_lasty = event.x, event.y
-
-    def draw_background(self, event):
-        if self.bg_last_x:
-            self.canvas.create_line((self.bg_last_x, self.bg_last_y, event.x, event.y), fill='blue', width=10)
-        self.bg_last_x = event.x
-        self.bg_last_y = event.y
+        if self.class_val == constants.classes[constants.FOREGROUND]:
+            color = 'red'
+        elif self.class_val == constants.classes[constants.BACKGROUND]:
+            color = 'blue'
+        else:
+            return
+        self.canvas.create_line((self.last_x, self.last_y, event.x, event.y), fill=color, width=2)
+        self.last_x, self.last_y = event.x, event.y
+        self.annotations[self.class_val].append([event.x, event.y])
+    def populate_mask(self):
+        mask = -np.ones((self.height, self.width))
+        for key, val in self.annotations.items():
+            val = np.array(val)
+            mask[val[:, 1], val[:, 0]] = key
+        return mask
 
     def save(self):
-        self.image.save('output.jpg')
+        mask = self.populate_mask()
+        # TODO: implement loading (if exists), merging and saving
+        # self.image.save('output.jpg')
 
     def change_color(self):
         rgb, color_string = colorchooser.askcolor(initialcolor=self.color)
@@ -92,8 +95,8 @@ class App:
             self.color = color_string
             self.color_button['text'] = 'Color: ' + color_string
 
-    def change_figure(self, figure):
-        self.figure = figure
+    def change_class(self, val):
+        self.class_val = val
 
     def change_size(self, event=None):
         try:
