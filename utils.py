02ba3355939790c27b48990e4a87104894169c81
DdHms <32897961+DdHms@users.noreply.github.com>
2021-11-25 09:08:20 +0200

    Update utils.py
    adapt to python 3.5

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import os
 
 
 def get_paths(folder, idx):
-    slice_path = os.path.join(folder, f'image_{idx}.npy')
-    pred_path = os.path.join(folder, f'pred_{idx}.npy')
-    scribble_path = os.path.join(folder, f'scribble_{idx}.npy')
+    slice_path = os.path.join(folder, 'image_' + str(idx) + '.npy')
+    pred_path = os.path.join(folder, 'pred_' + str(idx) + '.npy')
+    scribble_path = os.path.join(folder, 'scribble_' + str(idx) +'.npy')
     return slice_path, pred_path, scribble_path
