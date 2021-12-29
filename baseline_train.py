36fdbce01948d20a33828c0c52ca28937b69c6d1
yedidya <hyams.work@gmail.com>
2021-12-29 10:01:15 +0200

    fix baseline_training

--- baseline_train.py
+++ baseline_train.py
@@ -7,17 +7,20 @@ import pandas as pd
 import skimage
 import tensorflow as tf
 from tensorflow.keras import backend as K
+from tensorflow.keras.callbacks import EarlyStopping
+
 from tensorflow.keras import optimizers
 from tqdm import tqdm
 
 import constants
-from models.architectures import unet2d_5, unet2d_8
+from models.architectures import models
 from utils.general_utils import folder_picker
 from utils.image_utils import normalize_image
+import platform
 
+np.random.seed(constants.SEED)
 pool_folder = folder_picker(initialdir=constants.DATA_DIR)
 
-import platform
 
 if 'macOS' in platform.platform():
     os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
@@ -32,45 +35,23 @@ if 'macOS' in platform.platform():
 
 
 def data_generator(batch_size, image_paths):
-    # if data_type == 'train':
-    #     df = pd.read_csv(os.path.join(pool_folder, 'train', 'priorities.csv'))
-    #     image_paths = list(df['paths'])
-    # else:
-    #     image_paths = [pathlib.Path(os.path.join(pool_folder, 'train'), file) for file in os.listdir(pool_folder) if
-    #                    'image_' in file]
 
     sample_image = np.load(image_paths[0])
     n_rows, n_cols, n_input_channels = sample_image.shape
     x = np.zeros((batch_size, n_rows, n_cols, n_input_channels))
     y = np.zeros((batch_size, n_rows, n_cols, constants.n_classes))
     while True:
-        if data_type == 'train':
-            df = pd.read_csv(os.path.join(pool_folder, 'train', 'priorities.csv'))
-            image_paths = list(df['paths'])
-            priorities = list(df['score'])
-            priorities = np.array(priorities) / np.sum(priorities)
-        else:
-            image_paths = [pathlib.Path(os.path.join(pool_folder, 'train'), file) for file in os.listdir(pool_folder) if
-                           'image_' in file]
-            priorities = np.array([1.] * len(image_paths)) / len(image_paths)
-
         for i in range(batch_size):
-            found_non_empty_scribble = False
-            while not found_non_empty_scribble:
-                image_path = np.random.choice(image_paths, p=priorities)
-                image_path = pathlib.Path(image_path)
-                if data_type == 'train':
-                    scribble_path = pathlib.Path(image_path.parent, image_path.name.replace('image_', 'scribble_'))
-                    true = np.load(scribble_path)
-                else:
-                    gt_path = pathlib.Path(image_path.parent, image_path.name.replace('image_', 'gt_'))
-                    true = np.load(gt_path)
+            image_path = np.random.choice(image_paths)
+            image_path = pathlib.Path(image_path)
 
-                if np.any(true):
-                    img = np.load(image_path)
-                    x[i] = normalize_image(img)
-                    y[i] = true.astype(int)
-                    found_non_empty_scribble = True
+            gt_path = pathlib.Path(image_path.parent, image_path.name.replace('image_', 'gt_'))
+            true = np.load(gt_path)
+
+            if np.any(true):
+                img = np.load(image_path)
+                x[i] = normalize_image(img)
+                y[i] = true.astype(int)
         yield x, y
 
 
@@ -90,6 +71,7 @@ def config_parser():
     parser.add_argument('--batch', type=int, help='batchsize')
     parser.add_argument('--lr', type=float, help='learning rate')
     parser.add_argument('--annotate_gt', action='store_true')
+    parser.add_argument('--model', type=str, help='model name')
     return parser
 
 
@@ -97,28 +79,31 @@ if __name__ == '__main__':
     parser = config_parser()
     args = parser.parse_args()
 
-    training_generator = data_generator(batch_size=args.batch)
-    validation_generator = data_generator(batch_size=args.batch, data_type='val')
+    log_file = os.path.join(pool_folder, 'baseline_log.csv')
+    image_paths = [pathlib.Path(pool_folder, file) for file in os.listdir(pool_folder) if 'image_' in file]
+    np.random.shuffle(image_paths)
+    train_image_paths = image_paths[:int(args.train_p * len(image_paths))]
+    val_image_paths = image_paths[int(args.train_p * len(image_paths)) + 1 :]
+    n_train_full = len(train_image_paths)
+    n_val_full = len(val_image_paths)
+
+
+    training_generator = data_generator(batch_size=args.batch, image_paths=train_image_paths)
+    validation_generator = data_generator(batch_size=args.batch, image_paths=val_image_paths)
     x, y = next(training_generator)
     n_input_channels = x.shape[-1]
-    model = unet2d_5(n_input_channels)
+    model = models[args.model](n_input_channels)
     model.compile(loss=[weighted_cce], optimizer=optimizers.Adam(args.lr))
 
-    image_paths = [pathlib.Path(pool_folder, file) for file in os.listdir(pool_folder) if 'image_' in file]
-    pred_paths = [pathlib.Path(image_path.parent, image_path.name.replace('image_', 'pred_')) for image_path in
-                  image_paths]
-    gt_paths = [pathlib.Path(image_path.parent, image_path.name.replace('image_', 'gt_')) for image_path in
-                image_paths]
+    early_stopping_cbk = EarlyStopping(monitor='val_loss', mode='min', verbose=1)
 
-    while True:
+    for data_size in [0.1, 0.3, 0.5, 0.7, 0.9]:
+        training_generator = data_generator(batch_size=args.batch,
+                                            image_paths=train_image_paths[:int(data_size * n_train_full)])
+        validation_generator = data_generator(batch_size=args.batch, image_paths=val_image_paths)
         model.fit(training_generator, validation_data=validation_generator, steps_per_epoch=args.spe,
-                  epochs=args.epochs)
-        df = []
-        for image_path, pred_path in tqdm(zip(image_paths, pred_paths)):
-            image = np.load(image_path)
-            pred = model.predict(image[None, ...])[0]
-            p = priority_metric(pred)
-            df.append([image_path, p])
-            np.save(arr=pred, file=pred_path)
-        df = pd.DataFrame.from_records(df, columns=['paths', 'score'])
-        df.to_csv(constants.PRIORITY_DF)
+                  epochs=args.epochs, callbacks=[early_stopping_cbk])
+
+        loss_per_data_size = model.evaluate(validation_generator)
+        with open(log_file, 'a+') as log:
+            log.write(f'{int(data_size * n_train_full)},{loss_per_data_size}\n')
--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from tensorflow.keras import optimizers
 from tqdm import tqdm
 
 import constants
-from models.architectures import unet2d_5
+from models.architectures import models
 from utils.general_utils import folder_picker
 from utils.image_utils import normalize_image
 
@@ -94,6 +94,7 @@ def config_parser():
     parser.add_argument('--batch', type=int, help='batchsize')
     parser.add_argument('--lr', type=float, help='learning rate')
     parser.add_argument('--annotate_gt', action='store_true')
+    parser.add_argument('--model', type=str, help='model name')
     return parser
 
 
@@ -105,7 +106,7 @@ if __name__ == '__main__':
     validation_generator = data_generator(batch_size=args.batch, data_type='val')
     x, y = next(training_generator)
     n_input_channels = x.shape[-1]
-    model = unet2d_5(n_input_channels)
+    model = models[args.model](n_input_channels)
     model.compile(loss=[weighted_cce], optimizer=optimizers.Adam(args.lr))
 
     image_paths = [pathlib.Path(pool_folder, file) for file in os.listdir(pool_folder) if 'image_' in file]
